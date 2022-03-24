f181a46cb0b05f208fc993e7e80f0b95091261cc
andrew fauth <andrewlfauth@gmail.com>
2022-03-24 11:31:37 -0700

    style bug

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    'app/*/**/.{js,jsx}',
+    'app/*/**/*.{js,jsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'mont': ['"Montserrat", sans-serif']
+      },
+     
+    },
+  },
+  plugins: [],
+}
